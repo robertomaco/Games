3e4ab56910cec7bba0f1ddd9a5842a69097547bf
maccamcmacca <ry1nmca@gmail.com>
2018-03-25 18:28:50 +0100

    creates and prints board

--- chess.h
+++ chess.h
@@ -1,15 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct game{
+typedef struct boardDef{
 
     int board[8][8];
     
 
 } board;
 
-/* struct piece{
+typedef struct pieceDef{
 
-    piece 
+    int colour;
+    int rank;
 
-}; */
+}piece; 
+
+void initBoard(){
+
+    board mainBoard;
+
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            mainBoard.board[i][j] = 0;
+        }
+    }
+
+    //for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            mainBoard.board[1][j] = 02;
+        }
+    //}
+
+}
+
+void printBoard(){
+    board mainBoard;
+    
+    for(int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            printf("%d ", mainBoard.board[i][j]);
+        }
+        printf("\n");
+    }
+
+}
